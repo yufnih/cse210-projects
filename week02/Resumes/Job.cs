6bd225c287a2147c94992fee692628f55f33e23f
yufnih <okissanh@gmail.com>
2025-01-16 19:41:52 -0700

    Epic changes

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,19 @@
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company} {_startYear}-{_endYear})");
+    }
+}
+
+// Class: Job
+// _company : string
+// _jobTitle : string
+// _startYear : int
+// _endYear : int
+// Display() : void
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Graphic Designer";
+        job2._company = "Apple";
+
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);
+
     }
 }
+
+// Class: Job
+// _company : string
+// _jobTitle : string
+// _startYear : int
+// _endYear : int
+// Display() : void
+
+// Class : Resume
+// _name : string
+// _jobs : List<job>
+// Display() : void
